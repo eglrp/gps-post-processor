0958e500f125fffa47793a712e453ba8edb1ffc6
Michael Brandt <michael.j.brandt@colorado.edu>
2014-04-10 22:06:13 -0600

    typo

--- diff-gps/src/GpsPostProcessor.cpp
+++ diff-gps/src/GpsPostProcessor.cpp
@@ -9,7 +9,7 @@ int main(int argc, char* argv[]) {
   GpsPostProcessor& gpp = GpsPostProcessor::getInstance();
 
   // while loop or something for gpp to handle incoming data with a call to
-  // submitCsv() until it is shutodwn
+  // submitCsv() until it is shut down
 
   return 0;
 }
