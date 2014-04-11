9fadedcfc19c7c1bfe7cd76d90cf571e7a33231b
Michael Brandt <michael.j.brandt@colorado.edu>
2014-04-10 23:26:03 -0600

    whitespace consistency

--- diff-gps/src/ClientGpsData.h
+++ diff-gps/src/ClientGpsData.h
@@ -10,25 +10,25 @@ using namespace std;
 class ClientGpsData {
 
   private:
-		// TODO: fix these; we need consts for declaring length of member arrays,
-		// and we have them in our initial design, but what do they need to be?
-		//
-		// "N" replaces "0..n" from the diagram
-		// "M" replaces "0..m"
-		// "AXES" replaces "3", I'm pretty sure this was because it's for
-		//   3-dimensional coordinates
-		static const int N = 1;
-		static const int M = 1;
-		static const int AXES = 3;
+    // TODO: fix these; we need consts for declaring length of member arrays,
+    // and we have them in our initial design, but what do they need to be?
+    //
+    // "N" replaces "0..n" from the diagram
+    // "M" replaces "0..m"
+    // "AXES" replaces "3", I'm pretty sure this was because it's for
+    //   3-dimensional coordinates
+    static const int N = 1;
+    static const int M = 1;
+    static const int AXES = 3;
 
-		vector<int> time;
+    vector<int> time;
     double initialCart[N][AXES];
     double correctedCart[N][AXES];
     double initialGeo[N][AXES];
     double correctedGeo[N][AXES];
     bool satellites[N][M];
     int8_t flags[N];
-		string inUnits;
+    string inUnits;
     string outUnits;
     string nearestStationID;
     double stationCoords[AXES];
@@ -37,8 +37,8 @@ class ClientGpsData {
   public:
     ClientGpsData();
 
-		vector<int> getTime();
-		void setTime(const vector<int>&);
+    vector<int> getTime();
+    void setTime(const vector<int>&);
 };
 
 #endif
