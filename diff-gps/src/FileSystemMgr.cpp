70d5a27177712bc98d37906593d9f8cc20d0936e
wave <wave@wave-VirtualBox.(none)>
2014-04-27 17:08:23 -0600

    added minimal comments to FileSystemMgr.cpp to get better idea of what I need to do

--- diff-gps/src/FileSystemMgr.cpp
+++ diff-gps/src/FileSystemMgr.cpp
@@ -5,21 +5,21 @@ using namespace std;
 FileSystemMgr::FileSystemMgr(){}
 
 string FileSystemMgr::getRinexUrl(){ //string stationID, long ut
-	return"not done yet getRinexUrl";
+	return"not done yet getRinexUrl\n";
 }
 
 string FileSystemMgr::getAltRinexUrl(string stationID, long ut) {
-	return "not done yet getAltRinexUrl";
+	return "not done yet getAltRinexUrl\n";
 }
 
 string FileSystemMgr::getRinexDir(){
-	return "not done yet getRinexDir";
+	return "not done yet getRinexDir\n";
 }
 
 string FileSystemMgr::getRinexFileNames(string stationID, long ut){
-	return "not done yet getRinexFileNames";
+	return "not done yet getRinexFileNames\n";
 }
 string FileSystemMgr::getRinexPath(string stationID, long ut){
-	return "not done yet getRinexPath";
+	return "not done yet getRinexPath\n";
 }
 
